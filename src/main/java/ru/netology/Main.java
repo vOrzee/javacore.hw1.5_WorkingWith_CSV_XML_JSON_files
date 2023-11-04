a600306cd26c6294abf23ea830f6c495ac8c631b
vOrzee <104573735+vOrzee@users.noreply.github.com>
2023-11-04 22:47:26 +0500

    Complete Task 1.

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,7 +1,69 @@
 package ru.netology;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+//import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        String json = listToJson(list);
+        System.out.println(json);
+        writeString(json);
+    }
+
+    public static List<Employee> parseCSV(String[] columns, String fileName) {
+        List<Employee> result = new ArrayList<>();
+        ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+        strategy.setType(Employee.class);
+        strategy.setColumnMapping(columns);
+        try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(reader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            result = csv.parse();
+/*
+            // Массив считанных строк
+            String[] nextLine;
+            // Читаем CSV построчно
+            while ((nextLine = reader.readNext()) != null) {
+                // Работаем с прочитанными данными.
+                result.add(new Employee(Long.parseLong(nextLine[0]),nextLine[1],nextLine[2],nextLine[3],Integer.parseInt(nextLine[4])));
+            }
+*/
+        } catch (IOException /*| CsvValidationException*/ e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private static String listToJson(List<Employee> list) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.setPrettyPrinting().create();
+        Type listType = new TypeToken<List<Employee>>() {}.getType();
+        return gson.toJson(list, listType);
+    }
+
+    private static void writeString(String json) {
+        try (FileWriter file = new FileWriter("data.json")) {
+            file.write(json);
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
