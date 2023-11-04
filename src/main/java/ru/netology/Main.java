ffed70d5c69bb7607cbae9bfffba33cd861985b8
vOrzee <104573735+vOrzee@users.noreply.github.com>
2023-11-05 00:14:36 +0500

    Complete task 3.

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,7 +1,55 @@
 package ru.netology;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String fileName = "new_data.json";
+        String json = readString(fileName);
+        List<Employee> list = jsonToList(json);
+        for (Employee employee : list) {
+            System.out.println(employee);
+        }
+    }
+
+    private static String readString(String fileName) {
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+
+    private static List<Employee> jsonToList(String json) {
+        List<Employee> result = new ArrayList<>();
+        JSONParser parser = new JSONParser();
+        try {
+            JSONArray array = (JSONArray) parser.parse(json);
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            for (Object obj : array) {
+                JSONObject jsonObj = (JSONObject) obj;
+                Employee employee = gson.fromJson(jsonObj.toJSONString(), Employee.class);
+                result.add(employee);
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
